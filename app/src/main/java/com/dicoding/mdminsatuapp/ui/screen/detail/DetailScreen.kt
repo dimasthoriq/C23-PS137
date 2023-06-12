a138ef56ceb4b1ce2df46e2fcb045cd3157ec931
Okky Pratama <100081121+okkyPratama@users.noreply.github.com>
2023-06-12 18:25:18 +0700

    add detail activity layout

--- app/src/main/java/com/dicoding/mdminsatuapp/ui/screen/detail/DetailScreen.kt
+++ app/src/main/java/com/dicoding/mdminsatuapp/ui/screen/detail/DetailScreen.kt
@@ -1,15 +1,236 @@
 package com.dicoding.mdminsatuapp.ui.screen.detail
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.dicoding.mdminsatuapp.R
+import com.dicoding.mdminsatuapp.ui.components.PrimaryButton
+import com.dicoding.mdminsatuapp.ui.components.TopBar
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun DetailScreen() {
+    Scaffold(
+        topBar = {
+            TopBar(
+                text = "Detail Activity",
+                navigationIcon = Icons.Default.ArrowBack,
+                onNavigationIconClick = { /* Handle navigation icon click */ }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                val imageUrl =
+                    "https://raw.githubusercontent.com/okkyPratama/composedummyimages/main/data-dummy-compose-app/product_1.jpg"
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(16f / 9f)
+                ) {
+                    AsyncImage(
+                        model = imageUrl,
+                        contentDescription = "Activity Image",
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentScale = ContentScale.Crop,
+                    )
+                }
 
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    content = {
+                        Text(
+                            text = "Nama Aktivitas",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 24.sp
+                        )
+
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.End,
+                            content = {
+                                Text(
+                                    text = "20",
+                                    fontSize = 24.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color(0xFFFFA91A),
+                                    modifier = Modifier.size(36.dp)
+                                )
+                                Text(
+                                    text = "Kuota Left",
+                                    color = Color.Black,
+                                    modifier = Modifier.padding(start = 4.dp)
+                                )
+                            }
+                        )
+
+
+                    }
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = "Organized By: ",
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Nama Komunitas",
+                        textDecoration = TextDecoration.Underline
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "Deskripsi Aktivitas",
+                    fontSize = 16.sp,
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                ScheduleCard()
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                PrimaryButton(
+                    text = "Join Now",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    onClick = { /* Handle join button click */ }
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewDetailScreen() {
     DetailScreen()
+}
+
+@Composable
+fun ScheduleCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, top = 8.dp, end = 8.dp, bottom = 4.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .border(2.dp, Color(0xFFFFDE59), shape = RoundedCornerShape(16.dp)),
+        elevation = 4.dp
+    ) {
+        Box(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                modifier = Modifier
+                    .padding(8.dp),
+                verticalAlignment = Alignment.Top
+            ) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(end = 16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_calendar),
+                        contentDescription = "Calendar",
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "25",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                        Text(
+                            text = "Jan",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                    }
+                }
+                Box(
+                    modifier = Modifier
+                        .width(1.dp)
+                        .fillMaxHeight()
+                        .background(Color(0xFFFFA91A))
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column(
+                    modifier = Modifier
+                        .weight(3f)
+                        .padding(top = 8.dp, bottom = 4.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(text = "Monday", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(text = "09:00 - 11:00")
+                }
+            }
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(end = 8.dp, top = 8.dp)
+            ) {
+                OutlinedButton(
+                    onClick = { /* Handle button click */ },
+                    modifier = Modifier.padding(top = 0.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    border = BorderStroke(1.dp, Color(0xFFFFDE59)),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = Color.Transparent,
+                        contentColor = Color(0xFFFFDE59)
+                    )
+                ) {
+                    Text(
+                        text = "45 minutes",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewScheduleCard() {
+    ScheduleCard()
 }
